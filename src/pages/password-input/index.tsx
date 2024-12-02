940e067614e28f1da8c0de88eea97389414c0fbe
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2024-12-02 23:34:59 +0100

    add PasswordInput page

---
+++ src/pages/password-input/index.tsx
@@ -0,0 +1,11 @@
+import "./PasswordInput.css"
+
+function PasswordInput() {
+  return (
+    <div className="PasswordInput">
+      <input type="password" />
+    </div>
+  )
+}
+
+export default PasswordInput
