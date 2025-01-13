30906cd8e1307cfab699821c1965c3757498faa6
Mathieu Joly <m.joly@live.fr>
2025-01-13 20:57:04 +0100

    update color

--- src/pages/temperature-converter/src/components/OuterRing.tsx
+++ src/pages/temperature-converter/src/components/OuterRing.tsx
@@ -6,7 +6,7 @@ function OuterRing({ percentage }: { percentage: number }) {
         transform: `rotate(${45 + percentage * 3.6}deg)`,
         background: `conic-gradient(from 0.25turn, hsl(${
           percentage - 100
-        }, 87.5%, 93.73%), hsl(${percentage - 100}, 84.42%, 84.9%))`,
+        }, 87.5%, 93.73%), hsl(${percentage - 120}, 84.42%, 84.9%))`,
       }}
     />
   )
