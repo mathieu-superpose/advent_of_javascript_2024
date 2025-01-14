42e59403834f9a132a38251961e0af4d3c0b6ac1
Mathieu Joly <m.joly@live.fr>
2025-01-14 15:45:44 +0100

    add row / column sizing & gaps

--- src/pages/grid-generator/GridGeneratorPage.tsx
+++ src/pages/grid-generator/GridGeneratorPage.tsx
@@ -1,12 +1,17 @@
-import { SetStateAction, useState } from "react"
+import { SetStateAction, useEffect, useRef, useState } from "react"
 
 import "./GridGeneratorPage.css"
 
 function GridGeneratorPage() {
+  const gridRef = useRef<HTMLDivElement>(null)
+
   const [columns, setColumns] = useState(3)
   const [rows, setRows] = useState(3)
   const [columnGap, setColumnGap] = useState(0)
   const [rowGap, setRowGap] = useState(0)
+
+  const [rowDHeight, setRowWidth] = useState([...Array(12)].map((_r) => "1fr"))
+  const [colWidth, setColWidth] = useState([...Array(12)].map((_r) => "1fr"))
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement>,
@@ -29,6 +34,22 @@ function GridGeneratorPage() {
 
     setChange(num)
   }
+
+  useEffect(() => {
+    if (!gridRef?.current) {
+      return
+    }
+
+    const gridTemplateColumns = `repeat(${columns}, 1fr)`
+    gridRef.current.style.gridTemplateColumns = gridTemplateColumns
+
+    const gridTemplateRows = Array(rows).fill("1fr").join(" ")
+    gridRef.current.style.gridTemplateRows = gridTemplateRows
+
+    gridRef.current.style.columnGap = `${columnGap}px`
+
+    gridRef.current.style.rowGap = `${rowGap}px`
+  }, [rows, columns, columnGap, rowGap])
 
   return (
     <div className="GridGeneratorPage">
@@ -82,7 +103,16 @@ function GridGeneratorPage() {
         </ul>
       </div>
 
-      <div className="Grid"></div>
+      <div className="Playground">
+        <div className="Grid" ref={gridRef}>
+          {Array(columns * rows)
+            .fill(null)
+            .map((_, index) => (
+              <div key={index} className="Cell" />
+            ))}
+          <div className="Frame" />
+        </div>
+      </div>
     </div>
   )
 }
